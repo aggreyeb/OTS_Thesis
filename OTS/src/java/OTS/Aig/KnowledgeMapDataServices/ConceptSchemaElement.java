1c1c253a90af6a07087304f49a691f8df455aa24
Eb.Aggrey <Eb.Aggrey@gov.ab.ca>
2017-08-18 20:27:35 -0600

    Checked in missed files

---
+++ OTS/src/java/OTS/Aig/KnowledgeMapDataServices/ConceptSchemaElement.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OTS.Aig.KnowledgeMapDataServices;
+
+/**
+ *
+ * @author Eb
+ */
+public class ConceptSchemaElement {
+    String ConceptSchemaId;
+    String ConceptNodeId;
+    String RelationName;
+    String ConceptName;
+    String ActionName;
+    String AttributeName;
+    String AttributeValue;
+}
