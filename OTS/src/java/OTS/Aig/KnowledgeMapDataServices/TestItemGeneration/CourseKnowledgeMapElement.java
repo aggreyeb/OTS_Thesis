c9d697192d547e4b62b75e65caa1aeede70a434f
aggreyeb <aggreyeb@shaw.ca>
2017-08-21 07:40:59 -0600

    Implementing test generation user interface

---
+++ OTS/src/java/OTS/Aig/KnowledgeMapDataServices/TestItemGeneration/CourseKnowledgeMapElement.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OTS.Aig.KnowledgeMapDataServices.TestItemGeneration;
+
+/**
+ *
+ * @author MEA
+ */
+public class CourseKnowledgeMapElement {
+    public String KnowledgeMapId;
+    public String Name;
+    public String Description;
+    public String Concepts;//Pass only the nodes here
+}
